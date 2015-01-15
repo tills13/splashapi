cc8171b94c75e828a1551066fd8f6a213fbee09e
Tyler Sebastian <tills13@gmail.com>
2015-01-15 14:56:48 -0800

    small things

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,6 @@
 var after = 0;
 var container = null;
 var fetching = false;
-var print = console.log
 
 function percentWhiteHex(color) {
 	var groups = color.match(/#?([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})/)
@@ -24,11 +23,14 @@ function fetchPhotos() {
 		$.each(data, function(index, element) {
 			var image_container = $("<div></div>");
 			image_container.addClass("image-container");
+			image_container.attr("id", element.id);
 			image_container.css({ background: element.photo.color });
 
-			var fader = $("<div></div>");
+			var fader = $("<div></div>"), image_info = $("<span></span>");
 			fader.addClass("fader");
 			fader.css({ background: element.photo.color });
+			image_info.addClass("image-info");
+			image_info.text("#" + element.id);
 
 			var author = $("<div></div>");
 			var author_name = $("<div></div>"), author_url = $("<a></a>");
@@ -41,11 +43,13 @@ function fetchPhotos() {
 			if (percentWhiteHex(element.photo.color) < 55) {
 				author_name.css("color", "floralwhite");
 				author_url.css("color", "floralwhite");
+				image_info.css("color", "floralwhite");
 
 				author_url.css("border-bottom", "1px dotted floralwhite");
 			} else {
 				author_name.css("color", "#2B303B");
 				author_url.css("color", "#2B303B");
+				image_info.css("color", "#2B303B");
 
 				author_url.css("border-bottom", "1px dotted #2b303b");
 			}
@@ -56,11 +60,11 @@ function fetchPhotos() {
 			var image = $("<img>");
 
 			image.addClass("image");
-			image.attr("id", element.id);
 			image.attr("src", element.photo.url);
 			image.load(function() { $(this).css({ opacity: 1 }); });
 			
 			image_container.append(fader);
+			image_container.append(image_info);
 			image_container.append(author);
 			image_container.append(image);
 
@@ -75,7 +79,11 @@ function fetchPhotos() {
 	 });
 }
 
-$(document).ready(function() { container = $("body"); fetchPhotos(); });
+$(document).ready(function() { 
+	container = $("body");
+	fetchPhotos();
+});
+
 $(document).scroll(function() {
 	var offset = document.body.offsetHeight - window.pageYOffset - window.innerHeight;
 	if (offset < 750) fetchPhotos(); 
---
+++ static/js/splashapi.js
@@ -0,0 +1,44 @@
+//var baseURL = "https://splashapi.herokuapp.com";
+var baseURL = "http://localhost:5000";
+var randomURL = "/photos/v1.0/random";
+var listURL = "/photos/v1.0/list?count={%count}&after={%after}";
+var imageURL = "/photos/v1.0/image/{%id}";
+
+function SplashAPI() {}
+
+SplashAPI.prototype.getRandomPhoto = function(success) {
+	var httpRequest = new XMLHttpRequest();
+	var url = baseURL + randomURL;
+
+	httpRequest.addEventListener("load", function() {
+		if (this.status == 200 && success) success(JSON.parse(this.responseText));
+	}, false);
+
+	httpRequest.open("GET", url, true);
+	httpRequest.send();
+};
+
+SplashAPI.prototype.getPhoto = function(imageId, success) {
+	var httpRequest = new XMLHttpRequest();
+	var url = baseURL + imageURL.replace("{%id}", imageId);
+
+	httpRequest.addEventListener("load", function() {
+		if (this.status == 200 && success) success(JSON.parse(this.responseText));
+	}, false);
+
+	httpRequest.open("GET", url, true);
+	httpRequest.send();
+};
+
+SplashAPI.prototype.getPhotos = function(count, after, success) {
+	var httpRequest = new XMLHttpRequest();
+	var url = baseURL + listURL.replace("{%count}", count)
+								 .replace("{%after}", after);
+
+	httpRequest.addEventListener("load", function() {
+		if (this.status == 200 && success) success(JSON.parse(this.responseText));
+	}, false);
+
+	httpRequest.open("GET", url, true);
+	httpRequest.send();
+};
